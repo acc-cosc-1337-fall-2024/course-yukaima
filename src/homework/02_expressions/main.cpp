aae5304d8594b6630a048f785a62304037065cb6
yukaima <157077934+yukaima@users.noreply.github.com>
2024-02-06 00:34:04 +0000

    Homwork2

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -14,7 +14,19 @@ double main()
 	cout << "Please enter the meal amount: ";
 	cin >> meal_amount;
 
+	tax_amount = get_sales_tax_amount(meal_amount);
 
+	cout << "Please enter the tip rate: ";
+	cin >> tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount,tip_rate);
+
+	total = tip_amount + tax_amount + meal_amount;
+
+	cout << "Meal Amount: " << meal_amount << "\n";
+    cout << "Sales Tax: " << tax_amount << "\n";
+    cout << "Tip Amount: " << tip_amount << "\n";
+    cout << "Total: " << total << "\n";
 
 	return 0;
 }
