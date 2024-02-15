056864f7f60279ac896e436f243b1333269fed38
yukaima <157077934+yukaima@users.noreply.github.com>
2024-02-15 23:41:36 +0000

    decisions

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,60 @@
 //write include statement for decisions header
-
+#include "decisions.h"
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int grade)
+{
+    if( 90 <= grade && grade <= 100 )
+    {
+        return "A";
+    }
+    else if ( 80 <= grade )
+    {
+        return "B";
+    }
+    else if ( 70 <= grade )
+    {
+        return "C";
+    }
+    else if ( 60 <= grade )
+    {
+        return "D";
+    }
+    else if ( 00 <= grade )
+    {
+        return "F";
+    }
+    else
+    {
+        return "not found";
+    }
+    
+}
+string get_letter_grade_using_switch(int grade)
+{
+    switch ( grade / 10 )
+    {
+    case 10:
+    case 9:
+        return "A";
+        break;
+    case 8:
+        return "B";
+        break;
+    case 7:
+        return "C";
+        break;
+    case 6:
+        return "D";
+        break;
+    default:
+    return "F";
+        break;
+    }
+
+
+
+
+
+}
+
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,57 @@
 //write include statements
+#include <iostream>
+#include "decisions.h"
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
 
 int main() 
 {
+	int grade , Menu_choice;
+
+	cout << "Main Menu \n";
+    cout << "1-Letter grade using if \n";
+    cout << "2-Letter grade using switch \n";
+    cout << "3-Exit \n";
+
+	cout << "Enter the choice: ";
+	cin >> Menu_choice;
+
+	switch (Menu_choice)
+	{
+		case 1:
+	        cout << "Please enter your numerical grade here(0-100): ";
+		    cin >> grade;
+			if (00 <= grade && grade <= 100)
+			{
+				cout << "Your Letter grade is: " << get_letter_grade_using_if(grade);
+			}
+			else
+			{
+				cout << "the number is out of range";
+			}
+		    break;
+
+		case 2:
+	        cout << "Please enter your numerical grade here(0-100): ";
+		    cin >> grade;
+			if (00 <= grade && grade <= 100)
+			{
+				cout << "Your Letter grade is: " << get_letter_grade_using_switch(grade);
+			}
+			else
+			{
+				cout << "the number is out of range";
+			}
+		    break;
+		default:
+		    cout << "Exit";
+			break;
+	}
+	
+
+	
 	return 0;
 }
