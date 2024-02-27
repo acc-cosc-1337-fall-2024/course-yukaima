caee57e0be61f4ab41cdbb843cc2505de292bcd6
yukaima <157077934+yukaima@users.noreply.github.com>
2024-02-27 21:33:04 +0000

    homework

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,63 @@
 //add include statements
-
+#include "func.h"
 //add function code here
+
+double get_gc_content( const string& dna )
+{
+    int gc_count = 0;
+    int i ;
+    for ( i = 1; i <= dna.length(); i++)
+    {
+        char character = dna[i];
+        if (character == 'G' || character == 'C') 
+        {
+            gc_count++;
+        }
+    }
+
+    double gc_percent;
+    gc_percent =  (gc_count) / dna.length();
+
+    return gc_percent;
+}
+
+
+string get_reverse_string( string dna )
+{
+    string reversed_dna ;
+    int i = dna.length() ;
+    for ( i ; i >= 0; i--) 
+    {
+        reversed_dna = reversed_dna + dna[i];
+    }
+
+    return reversed_dna;
+}
+
+
+string get_dna_complement( string dna )
+{
+    string complement = dna;
+    int i ;
+    for ( i = 1; i <= dna.length(); i++)
+    {
+        char character = complement[i];
+        switch (character) 
+        {
+            case 'A':
+                character = 'T';
+                break;
+            case 'T':
+                character = 'A';
+                break;
+            case 'C':
+                character = 'G';
+                break;
+            case 'G':
+                character = 'C';
+                break;
+        }
+        complement[i] = character;
+    }
+    return complement;
+}
--- test/homework/05_functions/05_functions_tests.cpp
+++ test/homework/05_functions/05_functions_tests.cpp
@@ -1,7 +1,10 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "func.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+TEST_CASE("Test get_gc_content") 
+{
+	REQUIRE(get_gc_content( "AGCTATAG" ) == 0.375);
+	REQUIRE(get_gc_content( "CGCTATAG" ) == 0.50);
 }
 
